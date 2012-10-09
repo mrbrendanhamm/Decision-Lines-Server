f4951669cbad1f14ca799d2d4941f4740da9ce0d
azafty468 <azafty@gmail.com>
2012-10-09 15:13:17 -0400

    added the keyword "synchronized" to the AdminLogin controller and a database link

--- src/controller/AdminLogInController.java
+++ src/controller/AdminLogInController.java
@@ -1,13 +1,14 @@
 package controller;
 
 import shared.ClientState;
+import shared.DatabaseSubsystem;
 import shared.IProtocolHandler;
 import xml.Message;
 
 public class AdminLogInController implements IProtocolHandler {
 
 	@Override
-	public Message process(ClientState state, Message request) {
+	public synchronized Message process(ClientState state, Message request) {
 		// Initialize local variables
 		Message response = null;
 		boolean adminVerified=false;
@@ -17,9 +18,17 @@ public class AdminLogInController implements IProtocolHandler {
 		 */
 		String msgAdminID = null;
 		String msgAdminCredentials= null;;
+
+		// Andrew: instead of this how about use the database subsystem function call? 
+		if (DatabaseSubsystem.verifyAdminCredentials(msgAdminID,  msgAdminCredentials))
+			adminVerified = true;
+		else
+			adminVerified = false;
+		
 		/** Temporary store for a Admin and their Credentials. 
 		 *  Probably be in database realisticly.
 		 */
+		/*
 		String adminId = "TheAdmin123";	
 		String adminCredentials= "TheAdminPassword123";
 		
@@ -28,6 +37,7 @@ public class AdminLogInController implements IProtocolHandler {
 			adminVerified=true;
 		}
 		else adminVerified=false;
+		*/
 				
 		return response;
 	}
